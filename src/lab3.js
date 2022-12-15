e4934f51b3e10251b5fed19e7e16353f288969f5
Sa1zhan <119933737+Sa1zhan@users.noreply.github.com>
2022-12-15 15:06:49 +0600

    Add files via upload
    labs

---
+++ src/lab3.js
@@ -0,0 +1,7 @@
+function popUp(){
+    document.querySelector(".popup").style.display = "inline-display";
+}
+function popUp2(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup2").style.display = "inline-display";
+}
